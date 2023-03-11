e1006d5029a5c0a82f0b0740904f6ebc913fc0cf
sylvan452 <sylvan452@gmail.com>
2023-03-11 16:33:24 +0100

    day 2

--- Name_Generator/main.py
+++ Name_Generator/main.py
@@ -1,4 +1,6 @@
 print("Welcome to the Band Name Generator")
 city = input("What is the name of the ciyt you grew up in ?\n")
 pet = input("What is your pet name?\n")
-print("Your band name could be" + city " " + pet)
+print("Your band name could be" + city + wq
+i!!wq
+" " + pet)
