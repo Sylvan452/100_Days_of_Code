f3e29d79805165b8ec5f8bdebf068b8cfdbe1d69
sylvan452 <sylvan452@gmail.com>
2023-04-06 13:05:47 +0100

    swap

---
+++ jenny_lec/swap_number.py
@@ -0,0 +1,8 @@
+a = input('Enter a number for a: ')
+b = input('Enter a vaule for b: ')
+tmp = a
+a=b
+b=tmp
+
+print('a=' + a)
+print('b=' + b)
---
+++ jenny_lec/variable.py
@@ -0,0 +1,7 @@
+name = input('what is your name? ')
+print(name)
+print(len(name))
+
+name1 = 'Obah'
+print(name1)
+print(name + " " + name1)
