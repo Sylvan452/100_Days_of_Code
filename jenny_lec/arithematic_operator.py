f1f2d1398439e6014e9b22efac08a9d40fb489fb
sylvan452 <sylvan452@gmail.com>
2023-04-06 15:35:18 +0100

    arithematic oper

---
+++ jenny_lec/arithematic_operator.py
@@ -0,0 +1,13 @@
+print(4/2)
+print(4//2)
+print(2**2)
+print(5%2)
+print(5+3*4/2)
+
+
+weight=input('Enter your weight in kg: ')
+height=input('Enter your height in meter: ')
+weight = float(weight)
+height = float(height)
+BMI=weight /(height * 2)
+print(BMI)
