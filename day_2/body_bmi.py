aa42e6c8773ea6d1bcae2080223e2dce94c81750
sylvan452 <sylvan452@gmail.com>
2023-04-06 11:47:54 +0100

    print

---
+++ day_2/body_bmi.py
@@ -0,0 +1,4 @@
+height = int(input("enter your height in meter: "))
+weight = int(input("enter your weight in kg: "))
+bmi = weight/(height)**2
+print(bmi)
---
+++ day_2/first_1.py
@@ -0,0 +1,4 @@
+print('Hello world...')
+print('First Program - Python print function')
+print('It is declared like this:')
+print("print('what to print')")
