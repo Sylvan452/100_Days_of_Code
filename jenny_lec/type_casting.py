e3c1d8e1efd2d0481ed8885f1d98a9602735bb0e
sylvan452 <sylvan452@gmail.com>
2023-04-06 14:34:02 +0100

    type casting

---
+++ jenny_lec/type_casting.py
@@ -0,0 +1,9 @@
+lenght =len('Obah sylva')
+print('Your name has ' + str(lenght) + ' Characters')
+print(int('10') + int('10'))
+print(int('10') + float('10'))
+
+first_number=input('enter first number: ')
+second_number=input('enter second number: ')
+sum=int(first_number) + int(second_number)
+print(sum)
