8404d708b0f5b697d47696469b6af3077ea8d495
sylvan452 <sylvan452@gmail.com>
2023-04-06 20:12:18 +0100

    membership oper

---
+++ jenny_lec/membership_operator.py
@@ -0,0 +1,10 @@
+str = 'sylva obah'
+print('y' in str)
+print('Y' in str)
+print('lva' in str)
+print('s' not in str)
+print(' ' in str)
+
+num = [1, 10, 4, 7, 9]
+print(10 in num)
+print(4 not in num)
