306bdaae3cb01153f9eba466b2d94eb9d78e2af1
sylvan452 <sylvan452@gmail.com>
2023-04-08 16:24:04 +0100

    random module

---
+++ jenny_lec/random_module.py
@@ -0,0 +1,14 @@
+import random
+a=random.randint(1,9)
+print(a)
+b = random.randrange(1, 4)
+print(b)
+c = random.random()
+print(c)
+d = random.uniform(1, 3)
+print(d)
+l=[8, 9, 3, 2, 6, 8]
+e=random.choice(l)
+print(e)
+f=random.shuffle(l)
+print(l)
