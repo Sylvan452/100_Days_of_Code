f5a17c74b6e0342e06bcfdde24df70367b3b9666
sylvan452 <sylvan452@gmail.com>
2023-04-08 13:05:10 +0100

    nested if

---
+++ jenny_lec/nested_if.py
@@ -0,0 +1,11 @@
+number = int(input('Enter a number'))
+if number==1:
+    print('0ne')
+elif number==2:
+    print('Two')
+elif number==3:
+    print('Three')
+elif number==4:
+    print('Four')
+else:
+    print('above four')
