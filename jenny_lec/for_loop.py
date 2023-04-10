705292227f618d4dccd65a922aa1f12ffee9b23b
sylvan452 <sylvan452@gmail.com>
2023-04-10 16:14:00 +0100

    for loop

---
+++ jenny_lec/for_loop.py
@@ -0,0 +1,9 @@
+names={'sylva','doris','abigail','tessy','beauty','iruo'}
+for name in names:
+    print(name)
+    if name=='sylva':
+        print('hello !!!')
+
+my_name='sylva'
+for char in my_name:
+    print(char)
